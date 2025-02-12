338b4fbfbe9f0a361786a2db38a0ac89180bb895
Juan Cremades <relicary@gmail.com>
2025-02-12 15:24:01 +0100

    Added a loading message

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,7 +5,7 @@ import GifGrid from "./components/GifGrid";
 export const GifExpertApp = () => {
 
     const [ categories, setCategories ] = useState(
-        [ 'Dragon Ball' ]
+        [ 'Red Panda' ]
     );
 
     const onAddCategory = ( newCategory ) => {
